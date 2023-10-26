f560a58d2b0851c4978db6790c84c8d7e088f76b
ASHOK <ashoknagda790@gmail.com>
2023-10-26 19:57:47 +0530

    server one first

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //export const API_BASE_URL = "http://localhost:5454"
 //export const API_BASE_URL = "http://34.93.128.199:5454/:5454";
-export const API_BASE_URL="shopluxe.infinityfreeapp.com";
+export const API_BASE_URL="http://shopluxe.infinityfreeapp.com";
 const jwt = localStorage.getItem("jwt");
 
 export const api = axios.create({
